093904ca5cee8304ea8268c94e865e8914470314
finn <finn-hsv@online.de>
2023-10-29 15:25:22 +0100

    refactored QueryDeserializer

---
+++ MongoDocumentExporter.Test/Utils/BsonDeserializerTest.cs
@@ -0,0 +1,49 @@
+﻿using MongoDocumentExporter.Utils;
+using Xunit;
+
+namespace MongoDocumentExporter.Test.Utils;
+
+public class BsonDeserializerTest
+{
+    [Fact]
+    public async void ShouldReturnASimpleQuery()
+    {
+        // Given
+        var simpleQuery = "{\"createdAt\":\"2023-10-20\"}";
+        
+        // When
+        var result = await BsonDeserializer.Deserialize(simpleQuery);
+        
+        // Then
+        Assert.Equal("2023-10-20", result["createdAt"]);
+    }
+
+    [Fact]
+    public async void ShouldReturnAComplexQuery()
+    {
+        // Given
+        var complexQuery = "{\"createdAt\":{\"$in\":[\"1\",\"2\"]}, \"updatedAt\":\"2023-10-21T12:00:00.000Z\"}";
+        
+        // When
+        var result = await BsonDeserializer.Deserialize(complexQuery);
+        
+        // Then
+        Assert.Equal("{ \"$in\" : [\"1\", \"2\"] }", result["createdAt"].ToString());
+        Assert.Equal("2023-10-21T12:00:00.000Z", result["updatedAt"].ToString());
+    }
+
+    [Fact]
+    public async void ShouldThrowAFormatException()
+    {
+        // Given
+        var invalidQuery = "{-";
+        
+        // When
+        
+        // Then
+        await Assert.ThrowsAsync<FormatException>(async () =>
+        {
+            await BsonDeserializer.Deserialize(invalidQuery);
+        });
+    }
+}
---
+++ MongoDocumentExporter/Utils/BsonDeserializer.cs
@@ -0,0 +1,12 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+
+namespace MongoDocumentExporter.Utils;
+
+public class BsonDeserializer
+{
+    public static Task<BsonDocument> Deserialize(string bson)
+    {
+        return Task.FromResult(BsonSerializer.Deserialize<BsonDocument>(bson));
+    } 
+}
