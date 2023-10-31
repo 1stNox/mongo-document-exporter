4720bf4f4b144ffbcec60ade42324fe06a5b6b08
finn <finn-hsv@online.de>
2023-10-31 17:21:48 +0100

    implemented DocumentService without tests

--- MongoDocumentExporter/Services/DocumentService.cs
+++ MongoDocumentExporter/Services/DocumentService.cs
@@ -1,49 +1,25 @@
-﻿using System.Text.Json;
-using System.Text.Json.Serialization;
-using MongoDB.Bson;
-using MongoDB.Bson.IO;
+﻿using MongoDB.Bson;
 using MongoDB.Driver;
+using MongoDocumentExporter.Utils;
 
 namespace MongoDocumentExporter.Services;
 
 public class DocumentService
 {
-    private string DatabaseName { get; set; } = Environment.GetEnvironmentVariable("DATABASE_NAME") ??
-                                                    throw new NullReferenceException(
-                                                    "DATABASE_NAME environment variable is not set");
-
-    private string CollectionName { get; set; } = Environment.GetEnvironmentVariable("COLLECTION_NAME") ??
-                                                    throw new NullReferenceException(
-                                                        "COLLECTION_NAME environment variable is not set");
-
-    private string RawFilter { get; set; } = Environment.GetEnvironmentVariable("FILTER") ??
-                                                throw new NullReferenceException("FILTER environment variable is not set");
-
-    private string? RawSortOptions { get; set; } = Environment.GetEnvironmentVariable("SORT_OPTIONS");
-
-    private string? RawProjection { get; set; } = Environment.GetEnvironmentVariable("PROJECTION") ??
-                                                    throw new NullReferenceException("PROJECTION environment variable is not set");
-
+    private IMongoCollection<BsonDocument> Collection { get; set; }
     
-
-    private Task<BsonDocument> DeserializeFilter()
+    public DocumentService(IMongoCollection<BsonDocument> collection)
     {
-        var filter = JsonSerializer.Deserialize<object>(RawFilter).ToBsonDocument() ?? throw new NullReferenceException("FILTER environment variable could not be deserialized");
-        return Task.FromResult(filter);
+        Collection = collection;
     }
-
-    private Task<BsonDocument> DeserializeOptions()
+    
+    public async Task<List<BsonDocument>?> FindDocuments(string rawProjectionOptions, string? rawFilter, string? rawSortOptions)
     {
-        BsonDocument? sortOptions = null;
-        if (RawSortOptions != null)
-            sortOptions = JsonSerializer.Deserialize<object>(RawSortOptions!).ToBsonDocument();
-        var projectionOptions = JsonSerializer.Deserialize<object>(RawProjection!).ToBsonDocument() ?? throw new NullReferenceException("PROJECTION environment variable could not be deserialized");
-
-        var options = new BsonDocument();
-        if (sortOptions != null)
-            options["sort"] = sortOptions;
-        options["projection"] = projectionOptions;
-
-        return Task.FromResult(options);
+        var query = await QueryBuilder.Build(rawProjectionOptions, rawFilter, rawSortOptions);
+        
+        if (query.Sort is null)
+            return await Collection.Find(query.Filter).Project(query.Projection).ToListAsync();
+        
+        return await Collection.Find(query.Filter).Project(query.Projection).Sort(query.Sort).ToListAsync();
     }
 }
